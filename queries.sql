3b5878149cc3b4bc90b8a58027db1cccf8bed9c6
haroldmud <haroldmudosa@gmail.com>
2022-08-11 14:55:03 +0200

    update queries file

--- queries.sql
+++ queries.sql
@@ -58,3 +58,4 @@ SELECT Name, weight_kg FROM animals WHERE weight_kg = ( SELECT MAX(weight_kg) FR
 
 SELECT AVG( Escape_attempts ) AS escape_Average FROM animals WHERE Date_of_birth >= '1990-01-01' AND Date_of_birth <= '2000-01-01'; 
 
+jglkjvhf
